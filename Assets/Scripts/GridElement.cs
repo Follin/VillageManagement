cdf8b61a2a3bbc5ea22f06bf1a79e41caaa91ca6
Olivia Follin <odm.follin@gmail.com>
2019-11-07 11:11:40 +0100

    Game Set up + UI

---
+++ Assets/Scripts/GridElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridElement : MonoBehaviour
+{
+    [SerializeField] private int _gridId;
+    [SerializeField] private bool _isOccupied;
+
+    void Awake()
+    {
+        
+    }
+}
